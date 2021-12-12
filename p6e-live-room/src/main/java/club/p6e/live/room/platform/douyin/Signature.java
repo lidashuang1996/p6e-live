42bc1fcd2a1b705616264b1b4736776636680fff
lidashuang <1294935733@qq.com>
2021-12-12 19:51:41 +0800

    优化抖音对接的后台代码

--- p6e-live-room/src/main/java/club/p6e/live/room/platform/douyin/Signature.java
+++ p6e-live-room/src/main/java/club/p6e/live/room/platform/douyin/Signature.java
@@ -1,52 +1,210 @@
-//package club.p6e.live.room.platform.douyin;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-///**
-// * @author lidashuang
-// * @version 1.0
-// */
-//public final class Signature {
-//
-//    /** 参数地图 */
-//    private static final Map<String, Application> APPLICATION_MAP = new HashMap<>();
-//
-//    /**
-//     * 写入数据
-//     * @param application 应用程序
-//     */
-//    public synchronized static void set(Application application) {
-//        APPLICATION_MAP.put(application.getId(), application);
-//    }
-//
-//    /**
-//     * 删除数据
-//     * @param application 应用程序
-//     */
-//    public synchronized static void delete(Application application) {
-//        APPLICATION_MAP.remove(application.getId());
-//    }
-//
-//    /**
-//     * 执行
-//     * @param id 程序 ID
-//     * @param content 参数
-//     */
-//    public static void execute(String id, String content) {
-//        DyWebSocketHandler.sendMessage(id + "@@@" + content);
-//    }
-//
-//    /**
-//     * 执行后的回调
-//     * @param id 程序 ID
-//     * @param content 回调内容
-//     */
-//    public static void callback(String id, String content) {
-//        final Application application = APPLICATION_MAP.get(id);
-//        if (application != null) {
-//            application.execute(content);
-//        }
-//    }
-//
-//}
+package club.p6e.live.room.platform.douyin;
+
+import club.p6e.live.room.LiveRoomApplication;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author lidashuang
+ * @version 1.0
+ */
+public final class Signature {
+    /** 心跳超时时间 */
+    private static final long HEARTBEAT_DATE_TIME = 100000;
+    /** 心跳消息的内容 */
+    private static final String HEARTBEAT_CONTENT = "{\"type\":\"heartbeat\"}";
+
+    private static final Map<String, MessageCache> MESSAGE_CACHE = new HashMap<>();
+    private static final long MESSAGE_DATE_TIME = 60000;
+
+    public static void putMessageCache(String key, MessageCache value) {
+        synchronized (MESSAGE_CACHE) {
+            MESSAGE_CACHE.put(key, value);
+        }
+
+    }
+
+    public static void removeMessageCache(String key) {
+        synchronized (MESSAGE_CACHE) {
+            MESSAGE_CACHE.remove(key);
+        }
+    }
+
+    /**
+     * 执行
+     * @param id 程序 ID
+     * @param message 参数
+     */
+    public static void execute(String id, MessageCache message) throws IOException {
+        putMessageCache(id, message);
+        fetchWebSocketCache().sendMessage(new TextMessage(id + "@@@" + message.getContent()));
+    }
+
+    /**
+     * 执行后的回调
+     * @param id 程序 ID
+     * @param content 回调内容
+     */
+    public static void callback(String id, String content) {
+        try {
+            final MessageCache messageCache = MESSAGE_CACHE.get(id);
+            if (messageCache != null && messageCache.getCallback() != null) {
+                messageCache.getCallback().execute();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            removeMessageCache(id);
+        }
+    }
+
+    /** 签名程序封装对象 */
+    private static int WEBSOCKET_CACHE_INDEX = -1;
+    private static final String WEBSOCKET_LOCK = "lock";
+    private static final List<String> WEBSOCKET_LIST_CACHE = new ArrayList<>();
+    private static final Map<String, WebSocketCache> WEBSOCKET_MAP_CACHE = new HashMap<>();
+
+    static {
+        // 心跳任务创建
+        new LiveRoomApplication.Task(60, 60, true) {
+            @Override
+            public void execute() {
+                final long currentDateTime = System.currentTimeMillis();
+                try {
+                    for (final String key : WEBSOCKET_LIST_CACHE) {
+                        try {
+                            final WebSocketCache data = WEBSOCKET_MAP_CACHE.get(key);
+                            if (data != null) {
+                                if (data.getWebSocketSession() != null
+                                        && data.getWebSocketSession().isOpen()) {
+                                    if (data.getWebSocketDateTime() + HEARTBEAT_DATE_TIME >= currentDateTime) {
+                                        break;
+                                    }
+                                    data.getWebSocketSession().close();
+                                }
+                            }
+                            removeWebSocketCache(key);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    for (final String key : MESSAGE_CACHE.keySet()) {
+                        try {
+                            final MessageCache messageCache = MESSAGE_CACHE.get(key);
+                            if (!(messageCache != null &&
+                                    messageCache.getDateTime() + MESSAGE_DATE_TIME >= currentDateTime)) {
+                                removeMessageCache(key);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+
+
+
+    public static class MessageCache {
+
+        public interface Callback {
+            public void execute() throws Exception;
+        }
+
+        private final long dateTime;
+        private final String content;
+        private final Callback callback;
+
+        public MessageCache(String content, Callback callback) {
+            this.content = content;
+            this.dateTime = 60000;
+            this.callback = callback;
+        }
+
+        public long getDateTime() {
+            return dateTime;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public Callback getCallback() {
+            return callback;
+        }
+    }
+
+    public static class WebSocketCache {
+        private long webSocketDateTime;
+        private final WebSocketSession webSocketSession;
+
+        public WebSocketCache(WebSocketSession webSocketSession) {
+            this.webSocketSession = webSocketSession;
+            this.webSocketDateTime = System.currentTimeMillis();
+        }
+
+        public long getWebSocketDateTime() {
+            return webSocketDateTime;
+        }
+
+        public WebSocketSession getWebSocketSession() {
+            return webSocketSession;
+        }
+    }
+
+    public synchronized static WebSocketSession fetchWebSocketCache() {
+        final int size = WEBSOCKET_LIST_CACHE.size();
+        if (size == 0) {
+            throw new RuntimeException("no signer is available.");
+        } else {
+            WEBSOCKET_CACHE_INDEX = WEBSOCKET_CACHE_INDEX + 1;
+            if (WEBSOCKET_CACHE_INDEX >= size) {
+                WEBSOCKET_CACHE_INDEX = 0;
+            }
+            final String key = WEBSOCKET_LIST_CACHE.get(WEBSOCKET_CACHE_INDEX);
+            final WebSocketCache data = WEBSOCKET_MAP_CACHE.get(key);
+            if (data == null) {
+                removeWebSocketCache(key);
+                throw new RuntimeException("webSocketSession in data is empty.");
+            } else {
+                return data.getWebSocketSession();
+            }
+        }
+    }
+
+    public static void putWebSocketCache(String key, WebSocketCache value) {
+        synchronized (WEBSOCKET_LOCK) {
+            WEBSOCKET_LIST_CACHE.add(key);
+            WEBSOCKET_MAP_CACHE.put(key, value);
+        }
+    }
+
+    public static void removeWebSocketCache(String key) {
+        synchronized (WEBSOCKET_LOCK) {
+            WEBSOCKET_LIST_CACHE.remove(key);
+            WEBSOCKET_MAP_CACHE.remove(key);
+        }
+    }
+
+    public static void refreshWebSocketCache(String key) {
+        final WebSocketCache data = WEBSOCKET_MAP_CACHE.get(key);
+        if (data != null) {
+            data.webSocketDateTime = System.currentTimeMillis();
+        }
+    }
+}
--- p6e-live-room/src/main/java/club/p6e/live/room/platform/douyin/support/WebSocketHandler.java
+++ p6e-live-room/src/main/java/club/p6e/live/room/platform/douyin/support/WebSocketHandler.java
@@ -1,26 +1,18 @@
 package club.p6e.live.room.platform.douyin.support;
 
-import club.p6e.live.room.LiveRoomApplication;
+import club.p6e.live.room.platform.douyin.Signature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.*;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
 
 /**
  * @author lidashuang
- * @version 3.0
+ * @version 1.0
  */
 @Component
 public class WebSocketHandler implements org.springframework.web.socket.WebSocketHandler {
-
-    private static class Data {
-        private long webSocketDateTime = -1L;
-        private WebSocketSession webSocketSession;
-    }
 
     /** 注入日志对象 */
     private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketHandler.class);
@@ -30,69 +22,18 @@ public class WebSocketHandler implements org.springframework.web.socket.WebSocke
     private static final long HEARTBEAT_DATE_TIME = 100000;
     /** 心跳消息的内容 */
     private static final String HEARTBEAT_CONTENT = "{\"type\":\"heartbeat\"}";
-    /** 签名程序封装对象 */
-    private static final Map<String, Data> WEBSOCKET_CACHE = new HashMap<>();
 
-    static {
-        // 心跳任务创建
-        new LiveRoomApplication.Task(60, 60, true) {
-            @Override
-            public void execute() {
-                if (WS != null) {
-                    if (WS.isOpen()) {
-                        if (System.currentTimeMillis() + HEARTBEAT_DATE_TIME <= WS_DATE_TIME) {
-                            // 发送心跳
-                            sendMessage(HEARTBEAT_CHAR);
-                            return;
-                        }
-                    }
-                    try {
-                        WS.close();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        };
-    }
-
-    private static void putWebSocketCache(String key, Data value) {
-        synchronized (WEBSOCKET_CACHE) {
-            WEBSOCKET_CACHE.put(key, value);
-        }
-    }
-
-    private static void removeWebSocketCache(String key) {
-        synchronized (WEBSOCKET_CACHE) {
-            WEBSOCKET_CACHE.get(key);
-        }
+    @Override
+    public void afterConnectionEstablished(WebSocketSession webSocketSession) throws IOException {
+        // 添加缓存
+        Signature.putWebSocketCache(webSocketSession.getId(), new Signature.WebSocketCache(webSocketSession));
+        webSocketSession.sendMessage(new TextMessage("[ " + webSocketSession.getId() + " ] client connect success"));
+        LOGGER.info("[ DouYin afterConnectionEstablished ] client ID:  " +  webSocketSession.getId() + ", client is successfully connected.");
     }
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession webSocketSession) {
-        if (WS == null) {
-            WS = webSocketSession;
-            WS_DATE_TIME = System.currentTimeMillis();
-            sendMessage("[ " + webSocketSession.getId() + " ] client connect success");
-            LOGGER.info("[ " + webSocketSession.getId() + " ] client is successfully connected.");
-        } else {
-            if (!WS.isOpen()) {
-                WS = webSocketSession;
-                LOGGER.info("[ " + webSocketSession.getId() + " ] client is successfully connected.");
-            } else {
-                try {
-                    webSocketSession.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                LOGGER.error("[ " + webSocketSession.getId() + " ] client already exists, connection refused.");
-            }
-        }
-    }
-
-    @Override
-    public void handleMessage(WebSocketSession webSocketSession, WebSocketMessage<?> webSocketMessage) {
-        LOGGER.debug("[ DouYin handleMessage ] CLIENT ID: "
+    public void handleMessage(WebSocketSession webSocketSession, WebSocketMessage<?> webSocketMessage) throws IOException {
+        LOGGER.debug("[ DouYin handleMessage ] client ID: "
                 + webSocketSession.getId() + " ==> message: " + webSocketMessage.getPayload());
         if (webSocketMessage instanceof TextMessage) {
             final TextMessage textMessage = (TextMessage) webSocketMessage;
@@ -100,45 +41,30 @@ public class WebSocketHandler implements org.springframework.web.socket.WebSocke
             if (content.contains(CHAR)) {
                 final String[] cs = content.split(CHAR);
                 Signature.callback(cs[0], cs[1]);
-            } else if (HEARTBEAT_CHAR.equals(content)) {
-                WS_DATE_TIME = System.currentTimeMillis();
-                sendMessage(HEARTBEAT_CHAR);
+            } else if (HEARTBEAT_CONTENT.equals(content)) {
+                // 刷新缓存
+                Signature.refreshWebSocketCache(webSocketSession.getId());
+                webSocketSession.sendMessage(new TextMessage(HEARTBEAT_CONTENT));
             }
         }
     }
 
     @Override
     public void handleTransportError(WebSocketSession webSocketSession, Throwable throwable) {
-        LOGGER.error("[ DouYin handleTransportError ] CLIENT ID: "
+        LOGGER.error("[ DouYin handleTransportError ] client ID: "
                 + webSocketSession.getId() + " ==> throwable: " + throwable.getMessage());
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession webSocketSession, CloseStatus closeStatus) {
-        WS = null;
-        WS_DATE_TIME = -1;
-        LOGGER.info("[ DouYin afterConnectionClosed ] CLIENT ID: "
+        // 删除缓存
+        Signature.removeWebSocketCache(webSocketSession.getId());
+        LOGGER.info("[ DouYin afterConnectionClosed ] client ID: "
                 + webSocketSession.getId() + " ==> closeStatus: " + closeStatus);
     }
 
     @Override
     public boolean supportsPartialMessages() {
         return false;
-    }
-
-    /**
-     * 发送消息
-     * @param content 消息内容
-     */
-    public static void sendMessage(String content) {
-        try {
-            if (WS != null && WS.isOpen()) {
-                WS.sendMessage(new TextMessage(content));
-            } else {
-                throw new RuntimeException("web socket client not found.");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
